1eef017101fa2379c26f88d298df004ac2a45fe3
Santi-223 <santitsj223munera@gmail.com>
2024-02-27 10:12:50 -0500

    eliminar todo

--- Index.js
+++
@@ -1,8 +0,0 @@
-import App from "./App";
-
-const main = () =>{
-    App.listen(App.get("port"));
-    console.log(`Server on port ${App.get("port")}`);
-};
-
-main();
--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { methods as authRoutes } from "../controllers/auth.controller";
-
-const router = Router();
-
-router.post("/login", authRoutes.login);
-
-
-export default router;
