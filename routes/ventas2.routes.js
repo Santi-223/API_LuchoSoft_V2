6e29284b20f0d951e7f62dd085fee586879400bb
Juanda0125 <128322714+Juanda0125@users.noreply.github.com>
2024-06-09 08:27:58 -0500

    Update ventas2.routes.js

--- routes/ventas2.routes.js
+++ routes/ventas2.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
 import { methods as ventasControllers } from "../controllers/ventas2.controllers";
+
+const multer = require("multer")
+
+const upload = multer ({ dest : "uploads" })
+
 const router = Router();
 
 
@@ -12,18 +17,23 @@ router.get("/categoria_productos/:id_categoria_productos", ventasControllers.get
 
 
 
-router.post("/productos", ventasControllers.postProducto);
+router.post("/productos",  upload.single('imgProducto'), ventasControllers.postProducto);
 router.post("/categoria_productos", ventasControllers.postCategoria_producto);
 
 
 
-router.put("/productos/:id_producto", ventasControllers.updateProducto);
+router.put("/productos/:id_producto", upload.single('imgProducto'), ventasControllers.updateProducto);
 router.put("/categoria_productos/:id_categoria_productos", ventasControllers.updateCategoria_producto);
 
 
 
 router.delete("/productos/:id_producto", ventasControllers.deleteProducto);
 router.delete("/categoria_productos/:id_categoria_productos", ventasControllers.deleteCategoria_producto);
+
+router.put("/estadoProducto/:id_producto", ventasControllers.updateEstadoProductos)
+router.put("/estadoCategoria/:id_categoria_productos", ventasControllers.updateEstadoCategorias)
+
+
 
 
 
