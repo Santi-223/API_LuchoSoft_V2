e744bda46fa0646940b940fe72c408b58a973583
Santi-223 <santitsj223munera@gmail.com>
2024-06-18 15:22:00 -0500

    cambios mail

--- controllers/nodemailer.controller.js
+++ controllers/nodemailer.controller.js
@@ -36,20 +36,27 @@ const postCorreo = async (req, res) => {
             const token = await generarJwtRecuperacion(email)
 
             await transporter.sendMail({
-                from: '"Recuperar contraseña" <luchosoft3@gmail.com>', // sender address
+                from: '"luchosoft3@gmail.com" <luchosoft3@gmail.com>', // sender address
                 to: email, // list of receivers
                 subject: "Recuperación de contraseña.", // Subject line
                 text: "Hello world?", // plain text body
-                html: `<b><a href='https://luchosoftreact.onrender.com/#/recuperarContrasena2/${token}'style="color: #ffffff; 
-                background-color: #007bff; 
-                padding: 10px 20px; 
-                text-decoration: none; 
-                border-radius: 5px; 
-                font-size: 16px; 
-                font-weight: bold; 
-                display: inline-block;">
-          Recuperar contraseña
-          </a></b>`, // html body
+                html: `<div style="background-color: rgba(102, 22, 22, 0.103); border-radius: 5vw;">
+    <div>
+        <center>
+            <img style="width: 80vw; max-width: 400px; margin-top: 3vw;" src="https://res.cloudinary.com/donirviw7/image/upload/v1718740318/coydyew7jwirfbfxth7d.png">
+            <div style="margin-top: 3vw;">
+                <h1 style="font-family:Roboto;">Recuperar contraseña</h1>
+                <p style="font-family:Roboto;">Para recuperar tu contraseña haz <a href='https://luchosoftreact.onrender.com/#/recuperarContrasena2/${token}'>click aquí.</a></p>
+            </div>
+        </center>
+
+    </div>
+    <footer style="padding-top: 5vw; padding-bottom: 3vw; text-align: center;">
+        <p>&copy; 2024 LuchoSoft. Todos los derechos reservados.</p>
+    </footer>
+</div>
+
+`
             });
 
             return res.json({ msg: 'El correo de recuperacion se ha enviado exitosamente.', token: token });
