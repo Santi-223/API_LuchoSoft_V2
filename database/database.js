34cd2586543a03ec64138c0b79839ea317625df6
Santi-223 <santitsj223munera@gmail.com>
2024-12-13 19:57:07 -0500

    pushv2 13/12

--- database/database.js
+++ database/database.js
@@ -1,19 +1,32 @@
-//Conexión a la base de datos.
+import mysql from 'mysql2/promise';
+import config from '../config';
 
-import mysql from "promise-mysql";
-import config from "../config";
+let connectionPool;
 
-const connection=mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.user,
-    password: config.password
-})
-
-const getConnection = () => {
-    return connection
-};
-
-module.exports = {
-    getConnection
+async function initializeConnectionPool() {
+  try {
+    connectionPool = await mysql.createPool({
+      host: config.host || 'autorack.proxy.rlwy.net',
+      user: config.user || 'root',
+      password: config.password || 'OVEHaGCbtnIGFicSpvhpAdVcjWgqjdIY',
+      database: config.database || 'LuchoSoft',
+      port: config.port || 34063,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+    console.log('Pool de conexión inicializado con éxito');
+  } catch (error) {
+    console.error('Error al inicializar el pool de conexión:', error.message);
+    throw error;
+  }
 }
+
+async function getConnection() {
+  if (!connectionPool) {
+    await initializeConnectionPool();
+  }
+  return connectionPool.getConnection();
+}
+
+export { getConnection };
